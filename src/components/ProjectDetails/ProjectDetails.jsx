f366007b5d646d77864eea4a4eef7b8d529a86c8
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-01-26 21:00:03 +0530

    refac: remove unused code

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router";
+import React from "react";
 import "./ProjectDetails.css";
 import {
   convertNumberToLocaleString,
--- src/components/ProjectList/ProjectsTable.jsx
+++ src/components/ProjectList/ProjectsTable.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router";
 import { convertNumberToLocaleString } from "../../utils/utils";
 import Modal from "../Modal/Modal";
 import ProjectDetails from "../ProjectDetails/ProjectDetails";
